b72d33177194b95f337fb3c076a4e25b53899a27
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-08 12:51:59 +0200

    add app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import viewsStore, {Views} from "./store/viewsStore";
+import viewsStore, { Views } from "./store/viewsStore";
 import mainStore from "./store/store";
 import "./index.css";
 
-function App() {
-  const view = viewsStore(state => state.view);
-  const isFirstTime = mainStore(state => state.isFirstTime);
+import NavigationBar from "./components/NavigationBar";
+import Welcome from "./components/views/Welcome";
+import Wizard from "./components/views/Wizard";
+import Calendar from "./components/views/Calendar";
+import Settings from "./components/views/Settings";
 
-  return <>
-    {(isFirstTime && view === Views.Welcome) && <p>welcome</p>}
-    {view === Views.Wizard && <p>wizard</p>}
-    {view === Views.Calendar && <p>calendar</p>}
-    {view === Views.Settings && <p>settings</p>}
-  </>;
+function App() {
+  const view = viewsStore((state) => state.view);
+  const isFirstTime = mainStore((state) => state.isFirstTime);
+
+  return (
+    <>
+      {isFirstTime && view === Views.Welcome && <Welcome />}
+      {view === Views.Wizard && <Wizard />}
+      {view === Views.Calendar && <Calendar />}
+      {view === Views.Settings && <Settings />}
+      <NavigationBar />
+    </>
+  );
 }
 
 export default App;
