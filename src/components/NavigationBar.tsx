6fa499791d52207ac8f43d1ebd2d5314619d473d
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-08 12:51:22 +0200

    add navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,35 @@
+import { Views } from "../store/viewsStore";
+import viewsStore from "../store/viewsStore";
+
 export default function NavigationBar() {
-    return <nav className="fixed bottom-0 left-0 right-0 flex justify-center items-center gap-10">
-        <button>🗓️ Calendario</button>
-        <button>⚙️ Impostazioni</button>
+  const setView = viewsStore((state) => state.setView);
+
+  function handleViewSelector(e: React.MouseEvent) {
+    e.preventDefault();
+    const newView: Views = e.currentTarget.getAttribute("data-view") as Views;
+    setView(newView);
+  }
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 flex justify-center items-center gap-10">
+      <button
+        id={`select-${Views.Calendar}`}
+        onClick={handleViewSelector}
+        data-view={Views.Calendar}
+        type="button"
+        role="button"
+      >
+        🗓️ Calendario
+      </button>
+      <button
+        id={`select-${Views.Settings}`}
+        onClick={handleViewSelector}
+        data-view={Views.Settings}
+        type="button"
+        role="button"
+      >
+        ⚙️ Impostazioni
+      </button>
     </nav>
+  );
 }
