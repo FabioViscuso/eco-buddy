4dea153d682438c48e7ce9e7f00b1804f3757f89
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-09 22:19:03 +0200

    add icons

---
+++ src/utils/icons.ts
@@ -0,0 +1,17 @@
+import glass from "../assets/glass.png";
+import metal from "../assets/metal.png";
+import organic from "../assets/organic.png";
+import paper from "../assets/paper.png";
+import plastic from "../assets/plastic.png";
+import waste from "../assets/waste.png";
+
+const icons = {
+  glass,
+  metal,
+  organic,
+  paper,
+  plastic,
+  waste
+}
+
+export default icons;
