d94338a68b4954cadbb01a2ef7785029c07e4f69
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-12 20:16:58 +0200

    preliminary work for pwa transition

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({
+    manifest: {
+        name: 'RecyQ: L\'app per gestire il calendario della raccolta differenziata',
+        short_name: 'RecyQ',
+        start_url: '/',
+        background_color: '#242424',
+        theme_color: '#eee',
+        icons: [
+          {
+            src: '/images/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        // defining cached files formats
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      }
+    }
+  )],
+});
