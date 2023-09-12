9807ce0d86ad2877de24873f23e2b076ed5a0570
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-12 23:37:04 +0200

    implement full pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,37 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
+    injectRegister: 'script',
+    includeAssets: ['favicon.png', 'apple-touch-icon.png', 'mask-icon.png'],
     manifest: {
-        name: 'RecyQ: L\'app per gestire il calendario della raccolta differenziata',
-        short_name: 'RecyQ',
-        start_url: '/',
-        background_color: '#242424',
-        theme_color: '#eee',
-        icons: [
-          {
-            src: '/images/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/images/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        // defining cached files formats
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
-      }
+      name: 'Eco Buddy: Calendario per la raccolta differenziata',
+      short_name: 'Eco Buddy',
+      description: 'App per la creazione di un calendario di raccolta settimanale o mensile, con promemoria tramite notifiche push',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
     }
-  )],
+  })],
 });
