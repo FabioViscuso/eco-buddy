4bdd759bc42fc4ff4701d617758bf7afc225cc9b
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-09-12 22:25:56 +0200

    fix text alignment

--- src/components/views/Welcome.tsx
+++ src/components/views/Welcome.tsx
@@ -10,7 +10,7 @@ export default function Welcome() {
       className="h-full w-full flex flex-col justify-center items-center gap-10"
     >
       <h1 className="text-green-400 text-4xl">Ciao!</h1>
-      <p>
+      <p className="text-center">
         Con RecyQ puoi gestire il calendario della raccolta differenziata e, se
         vuoi, ricevere una notifica appena arriva il momento di conferirla.
       </p>
